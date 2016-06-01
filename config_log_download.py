092917994eec91d51699433bc006148a499f8581
U425651 <daedalus.chan@wellsfargo.com>
2016-06-01 16:22:16 +0800

    Initial commit

---
+++ config_log_download.py
@@ -0,0 +1,26 @@
+#################################################
+# A tmp location where files will be
+# temporarily stored / compressed.
+#################################################
+remote_work_dir = r'/tmp/daedalus'
+
+#################################################
+# The file name of the log file
+#################################################
+prm_log_name = 'application.log'
+
+#################################################
+# The log file name together with the full path
+#################################################
+remote_log_dir = r'/apps/myApps/logs/' + prm_log_name
+
+#################################################
+# hostname or i.p. address of the remote server
+#################################################
+remote_host = 'host-dns.com'
+
+#################################################
+# The user name that used to login (ssh) on the
+# remote server
+#################################################
+remote_usr = 'daedalus'
---
+++ log_download.py
@@ -0,0 +1,42 @@
+from config_log_download import *
+import paramiko, getpass, gzip, os
+from os import remove
+
+
+try:
+    remove(prm_log_name)
+except FileNotFoundError:
+    pass
+try:
+    remove(prm_log_name + '.gz')
+except FileNotFoundError:
+    pass
+
+password = getpass.getpass(prompt='pwd:')
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(hostname=remote_host, username=remote_usr, password=password)
+stdin, stdout, stderr = ssh.exec_command('uptime')
+
+print(stdout.readline())
+
+stdin, stdout, stderr = ssh.exec_command('rm -f ' + remote_work_dir + r'/*')
+stdin, stdout, stderr = ssh.exec_command('cp ' + remote_log_dir + r' ' + remote_work_dir)
+stdin, stdout, stderr = ssh.exec_command('gzip ' + remote_work_dir + r'/' + prm_log_name)
+stdin, stdout, stderr = ssh.exec_command('ls -lF ' + remote_work_dir)
+
+for line in stdout.read().splitlines():
+    print(line)
+
+ftp = ssh.open_sftp()
+ftp.get(remote_work_dir + r'/' + prm_log_name + r'.gz', prm_log_name + r'.gz')
+
+ftp.close()
+ssh.close()
+
+inF = gzip.open(prm_log_name + '.gz', 'rb')
+outF = open(prm_log_name, 'wb')
+outF.write(inF.read() )
+inF.close()
+outF.close()
